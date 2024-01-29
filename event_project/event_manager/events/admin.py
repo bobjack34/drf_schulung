e0fb8a7a6aaed62899e34f6ae71703360e470380
bobjack34 <you@example.com>
2024-01-29 14:13:25 +0100

    admin actions

--- event_project/event_manager/events/admin.py
+++ event_project/event_manager/events/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Category, Event 
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "sub_title"]  # liste von Feldern in der Übersicht
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+
+    @admin.action(description="Mache aktive")
+    def make_active(self, _, queryset):
+        # queryset => Objekte, die für die Aktion ausgewählt wurden (per checkbox)
+        # setze alle Einträge im Queryset auf is_active = True
+        queryset.update(is_active=True)
+
+    def make_inactive(self, _, queryset):
+        queryset.update(is_active=False)
+    
+    # Registreieren der Aktionen
+    actions = ["make_active", "make_inactive"]
+
+    # Anzeige in der Übersicht
+    list_display = ["name", "category", "min_group"]
+
+    # generiert eine Suchbox (erscheint in admin)
+    search_fields = ["name", "description"]
+    list_filter = ["category"]
--- event_project/event_manager/events/models.py
+++ event_project/event_manager/events/models.py
@@ -3,6 +3,17 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     """Model für eine Kategorie. Jedes Model muss von models.Model erben."""
+
+    class Meta:
+        # Meta-Informationen über das Model
+        verbose_name_plural = "Kategorien"
+        verbose_name = "Kategorie"
+
+    created_at = models.DateTimeField(auto_now_add=True)  # Zeitstempel bei Anlegen setzen
+
+    # immer, wenn Datensatz geändert wird, wird updated_at auf den Zeitstempel gesetzt
+    updated_at = models.DateTimeField(auto_now=True) 
+
     name = models.CharField(max_length=100)  # mandatory, VARCHAR 100
     # null=True => darf in der DB NULL sein
     # blank=True => darf im Formular leer sein
@@ -16,4 +27,27 @@ class Category(models.Model):
         mit ihrem Namen ausgegeben werden."""
         return self.name
 
+
+class Event(models.Model):
+
+    class MinGroup(models.IntegerChoices):
+        """Select-Box in der Adminoberfläche"""
+        SMALL = 2
+        MEDIUM = 5
+        BIG = 10
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    min_group = models.IntegerField(choices=MinGroup.choices)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+    is_active = models.BooleanField(default=True)
+     
+
+    def __str__(self) -> str:
+        return self.name
 
